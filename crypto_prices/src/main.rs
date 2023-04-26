4b8e27c93a75b12c057d334a56f8f2f02a5feced
NisarLDev <HarusBadah@proton.me>
2023-04-26 11:15:20 +0200

    Update main function. Added error management and reading number of bytes of input

--- crypto_prices/src/main.rs
+++ crypto_prices/src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     let mut coin: String = String::new();
     let result: Result<usize, Error> = std::io::stdin().read_line(buf: &mut coin);
     match result {
-        Ok(nro_bytes: usize) => println!("{nro_bytes}"),
-        Err() => todo!(),
+        Ok(nro_bytes: usize) => println!("Number of bytes readed{nro_bytes}"),
+        Err() => println!!("Error ocurred"),
     }
 }
